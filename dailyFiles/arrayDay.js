807093dbf4d28ffd4c1c09dcf875988590d45764
Jillian Flook <jkenw10@gmail.com>
2017-07-23 14:55:21 -0500

    finished array method recap

--- dailyFiles/arrayDay.js
+++ dailyFiles/arrayDay.js
@@ -53,8 +53,28 @@ inventors.sort(function(a,b){
 });
 
 
-//create list of blvds in Paris that contain "da" in the name. 
+//Create list of blvds in Paris that contain "da" in the name. 
 //https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris 
 
+const links = Array.from(document.querySelectorAll(".mw-category a")) //querySelector returns nodelist and must convert to get an array that can take map. 
+const deArray = links.map(link => link.textContent).filter(link => (link.includes("de")));
 
-//
+
+
+//Sort people by last name
+const newPpl = people.sort(function(a,b){
+	//const parts = a.split(", ") //returns the name in one object with first[0] and last[1]
+	const [aLast, aFirst] = a.split(", ");
+	const [bLast, bFirst] = b.split(", ");
+	return aLast > bLast ? -1 : 1;
+});
+
+
+//Sum reoccuring instances of each item 
+const transport = ["bike", "car", "bike", "car", "bike", "car", "bus", "walk", "board", "walk", "board"]
+
+const instances = transport.reduce((obj, item) =>{
+	!obj[item] ? obj[item] = 1 : obj[item]++;
+    return obj;
+}, {}) //set empty object that will hold instances
+
